a303b93552439b22e8f71c320ad88a8bceed048f
rahulsharma09 <08rahul.sharma01@gmail.com>
2024-06-21 18:35:36 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const parent = React.createElement("div" , {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I'm an h1 tag")
-    ])
-])
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div>
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All" />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return(
+        <div className="app">
+            <Header />
+          <h1>Hello</h1>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout />);
